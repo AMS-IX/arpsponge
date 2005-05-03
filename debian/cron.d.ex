8c94914696a1d324ad3d5c69c2d569c6bf8662ff
Steven Bakker <steven.bakker@ams-ix.net>
2005-05-03 14:37:16 +0000

    Added Debian dir to repository.

---
+++ debian/cron.d.ex
@@ -0,0 +1,4 @@
+#
+# Regular cron jobs for the arpswiffer package
+#
+0 4	* * *	root	arpswiffer_maintenance
---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(arpswiffer):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
+  title="arpswiffer" command="/usr/bin/arpswiffer"
