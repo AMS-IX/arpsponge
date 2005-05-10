578047cc511d9ab66be63401e9da0128d9206865
Steven Bakker <steven.bakker@ams-ix.net>
2005-05-10 12:26:42 +0000

    3.4.1

--- rules.mk
+++ rules.mk
@@ -10,7 +10,7 @@ default : all
 RM            =  /bin/rm -f
 MV            =  /bin/mv
 
-RELEASE       =  3.4
+RELEASE       =  3.4.1
 NAME          =  arpsponge
 PACKAGE       =  $(NAME)-$(RELEASE)
 TOOLDIR       =  $(TOPDIR)/tools
