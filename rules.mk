083a424f9abfccc5a9b33459731388fd30ac551e
Steven Bakker <steven.bakker@ams-ix.net>
2010-08-10 15:20:24 +0000

    Let the install/remove of init links be handled automatically.

--- rules.mk
+++ rules.mk
@@ -10,7 +10,7 @@ default : all
 RM            =  /bin/rm -f
 MV            =  /bin/mv
 
-RELEASE       =  3.9.4
+RELEASE       =  3.9.5
 NAME          =  arpsponge
 PACKAGE       =  $(NAME)-$(RELEASE)
 TOOLDIR       =  $(TOPDIR)/tools
