449b1cfbed61d2b8ac3b2f9693ad50a93dc2e313
Steven Bakker <steven.bakker@ams-ix.net>
2011-04-26 14:51:20 +0000

    Bumpy version. Update Changelog re: ARP cache updates.

--- rules.mk
+++ rules.mk
@@ -10,7 +10,7 @@ default : all
 RM            =  /bin/rm -f
 MV            =  /bin/mv
 
-RELEASE       =  3.10-beta
+RELEASE       =  3.10-beta2
 NAME          =  arpsponge
 PACKAGE       =  $(NAME)-$(RELEASE)
 TOOLDIR       =  $(TOPDIR)/tools
