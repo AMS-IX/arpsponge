4069639f536aa76728233b6c9f98fd6f341a5924
Steven Bakker <steven.bakker@ams-ix.net>
2011-06-14 10:10:28 +0000

    Bumpy.

--- rules.mk
+++ rules.mk
@@ -10,7 +10,7 @@ default : all
 RM            =  /bin/rm -f
 MV            =  /bin/mv
 
-RELEASE       =  3.10-beta6
+RELEASE       =  3.10-beta7
 NAME          =  arpsponge
 PACKAGE       =  $(NAME)-$(RELEASE)
 TOOLDIR       =  $(TOPDIR)/tools
