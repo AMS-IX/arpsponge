72ca6d2fece0ae04ade18aa0415f3e6bb9946f58
Steven Bakker <steven.bakker@ams-ix.net>
2011-08-23 13:27:22 +0000

    Version bump.

--- rules.mk
+++ rules.mk
@@ -10,7 +10,7 @@ default : all
 RM            =  /bin/rm -f
 MV            =  /bin/mv
 
-RELEASE       =  3.10
+RELEASE       =  3.11
 NAME          =  arpsponge
 PACKAGE       =  $(NAME)-$(RELEASE)
 TOOLDIR       =  $(TOPDIR)/tools
