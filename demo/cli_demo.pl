3d3cd9af22fee81867f8e3d94ddf8ae1d8af051e
Steven Bakker <steven.bakker@ams-ix.net>
2016-01-23 22:36:24 +0100

    Fixed cli_demo.pl to use new OO interface.

--- demo/cli_demo.pl
+++ demo/cli_demo.pl
@@ -1,45 +1,48 @@
- use Modern::Perl;
- use lib qw( .. );
- use Data::Dumper;
+#!/usr/bin/perl
 
- use FindBin;
- use M6::CLI qw( :all );
+use Modern::Perl;
+use lib qw( ../lib );
+use Data::Dumper;
 
- my $prog = $FindBin::Script;
- init_readline(
-            'history_lines' => 1000,
-            'completion'    => \&M6::CLI::complete_line,
-            'name'          => $prog,
-            'history_file'  => "$::ENV{HOME}/.${prog}_history",
-        );
+use FindBin;
+use M6::CLI;
 
- my $syntax = compile_syntax({
+my $prog = $FindBin::Script;
+my $term = M6::CLI->new(name => 'cli_demo', history_lines => 3);
+$term->read_history();
+
+my $syntax = $term->compile_syntax({
     'quit' => { '?' => 'Exit program.' },
     'help' => { '?' => 'Show command summary.' },
-    'ping $count? $delay?' => {
+    'show status|performance|parameters|version|verbose|vitals' => {
+        '?' => 'show stuff',
+    },
+    'ping $count? $delay? $fname?' => {
         '?'      => 'Send "ping" packets, display RTT.',
         '$count' => { type=>'int', min=>1, default=>1 },
         '$delay' => { type=>'float', min=>0.01, default=>1 },
-    }
- });
+        '$fname' => { type=>'filename', default=>'' },
+    },
+});
 
- print Dumper($syntax);
+print Dumper($syntax);
+my $FEAT = $term->term->Features;
+print Dumper($FEAT);
 
- my @parsed;
- my %args;
- while (1) {
-    my $input = $TERM->readline('~> ');
+say "history size: ", $term->history_lines;
+
+my %args;
+while (1) {
+    my $input = $term->readline('~> ');
     last if !defined $input;
 
     next if $input =~ /^\s*(?:#.*)?$/;
 
-    if (parse_line($input, \@parsed, \%args)) {
+    my @parsed;
+    if ($term->parse_line($input, \@parsed, \%args)) {
         print "@parsed\n";
         print Dumper(\%args);
     }
-    else {
-        print "ERROR\n";
-    }
- }
+}
 
- exit_readline();
+$term->write_history();
