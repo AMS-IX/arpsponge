f38bb5677c4e22269bf6649f347a735b58f361f3
Steven Bakker <steven.bakker@ams-ix.net>
2018-06-18 17:17:36 +0200

    The asctl option is --interface, not --device. :-(
    Closes: #2

--- init.d/arpsponge.sh
+++ init.d/arpsponge.sh
@@ -140,7 +140,7 @@ start_sponge() {
 
         if [ "$mode" = "re-init" ] && [ -f "${rundir}/status" ]
         then
-            ${BINDIR}/asctl --device="${DEVICE}" -c load status "${rundir}/status"
+            ${BINDIR}/asctl --interface="${DEVICE}" -c load status "${rundir}/status"
         fi
     )
 }
