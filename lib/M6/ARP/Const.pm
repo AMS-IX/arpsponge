f642905af7059527cbb3ce931db0f81db0b0cc98
Steven Bakker <steven.bakker@ams-ix.net>
2018-11-08 23:07:42 +0100

    Speed improvements
     * Eliminate argument shifting.
     * Eliminate else.

--- lib/M6/ARP/Const.pm
+++ lib/M6/ARP/Const.pm
@@ -27,25 +27,28 @@ use base qw( Exporter );
 BEGIN {
     our $VERSION = 1.02;
 
-    my @func   = qw( parse_update_flags update_flags_to_str is_valid_state state_to_string );
+    my @func   = qw(
+        parse_update_flags update_flags_to_str is_valid_state
+        state_to_string 
+    );
     my @states = qw( STATIC DEAD ALIVE PENDING NONE );
     my @update_flags = qw(
-                ARP_UPDATE_REPLY
-                ARP_UPDATE_REQUEST
-                ARP_UPDATE_GRATUITOUS
-                ARP_UPDATE_NONE
-                ARP_UPDATE_ALL
-            );
+        ARP_UPDATE_REPLY
+        ARP_UPDATE_REQUEST
+        ARP_UPDATE_GRATUITOUS
+        ARP_UPDATE_NONE
+        ARP_UPDATE_ALL
+    );
 
     our @EXPORT_OK = ( @func, @states, @update_flags );
     our @EXPORT    = ();
 
     our %EXPORT_TAGS = ( 
-            'func'   => \@func,
-            'states' => \@states,
-            'flags'  => \@update_flags,
-            'all'    => [ @func, @states, @update_flags ]
-        );
+        'func'   => \@func,
+        'states' => \@states,
+        'flags'  => \@update_flags,
+        'all'    => [ @func, @states, @update_flags ]
+    );
 }
 
 use constant ARP_UPDATE_REPLY      => 0x01;
@@ -55,16 +58,16 @@ use constant ARP_UPDATE_NONE       => 0x00;
 use constant ARP_UPDATE_ALL        => 0x07;
 
 our %UPDATE_FLAG_TO_STR = (
-        ARP_UPDATE_REPLY()      => 'reply',
-        ARP_UPDATE_REQUEST()    => 'request',
-        ARP_UPDATE_GRATUITOUS() => 'gratuitous',
-    );
+    ARP_UPDATE_REPLY()      => 'reply',
+    ARP_UPDATE_REQUEST()    => 'request',
+    ARP_UPDATE_GRATUITOUS() => 'gratuitous',
+);
 
 our %STR_TO_UPDATE_FLAG = (
-        'none' => ARP_UPDATE_NONE,
-        'all'  => ARP_UPDATE_ALL,
-         map { ($UPDATE_FLAG_TO_STR{$_} => $_) } keys %UPDATE_FLAG_TO_STR,
-    );
+    'none' => ARP_UPDATE_NONE,
+    'all'  => ARP_UPDATE_ALL,
+        map { ($UPDATE_FLAG_TO_STR{$_} => $_) } keys %UPDATE_FLAG_TO_STR,
+);
 
 # State constants/macros
 use constant STATIC  => -3;
@@ -86,51 +89,42 @@ our %STR_TO_STATE = (
 
 =over
 
-=item X<state_to_string>B<state_to_string> ( I<ARG> )
+=item B<state_to_string> ( I<ARG> )
+X<state_to_string>
 
 =cut
 
 sub state_to_string {
-    my $state = shift;
+    my ($state) = @_;
 
-    if (!defined $state) {
-        return 'NONE';
-    }
-    elsif ($state < PENDING(0)) {
-        return $STATE_TO_STR{$state} // 'ILLEGAL';
-    }
-    else {
-        return sprintf("PENDING(%d)", $state - PENDING(0));
-    }
+    return 'NONE' if !defined $state;
+    return $STATE_TO_STR{$state} // 'ILLEGAL' if $state < PENDING(0);
+    return sprintf("PENDING(%d)", $state - PENDING(0));
 }
 
 =over
 
-=item X<is_valid_state>B<is_valid_state> ( I<ARG>
-[, B<-err =E<gt>> I<REF> )
+=item B<is_valid_state> ( I<ARG> [, B<-err =E<gt>> I<REF> )
+X<is_valid_state>
 
 =cut
 
 sub is_valid_state {
-    my $arg = uc shift;
+    my $arg = uc $_[0];
     my $err_s;
-    my %opts = (-err => \$err_s, @_);
+    my %opts = (-err => \$err_s, @_[1..$#_]);
 
     $arg =~ s/^\s+//;
     $arg =~ s/\s+$//;
 
-    if (exists $STR_TO_STATE{$arg}) {
-        return $STR_TO_STATE{$arg};
-    }
-    else {
-        ${$opts{-err}} = q/"$arg" is not a valid state/;
-        return;
-    }
+    return $STR_TO_STATE{$arg} if exists $STR_TO_STATE{$arg};
+
+    ${$opts{-err}} = q/"$arg" is not a valid state/;
+    return;
 }
 
-=item X<parse_update_flags>B<parse_update_flags> ( I<ARG>
-[, B<-err> =E<gt> I<REF>]
-)
+=item B<parse_update_flags> ( I<ARG> [, B<-err> =E<gt> I<REF>] )
+X<parse_update_flags>
 
 Check whether I<ARG> represents a valid list of ARP update flags. Returns an
 integer representing the flags on success, C<undef> on error. Note that an
@@ -142,9 +136,9 @@ contain a diagnostic.
 =cut
 
 sub parse_update_flags {
-    my $arg = shift;
+    my ($arg, @opts) = @_;
     my $err_s;
-    my %opts = (-err => \$err_s, @_);
+    my %opts = (-err => \$err_s, @opts);
 
     my $flags = ARP_UPDATE_NONE;
     return $flags if ! defined $arg;
@@ -157,23 +151,23 @@ sub parse_update_flags {
             $method = 'all';
             $negate = !$negate;
         }
-        if (exists $STR_TO_UPDATE_FLAG{$method}) {
-            if ($negate) {
-                $flags &= ~ $STR_TO_UPDATE_FLAG{$method};
-            }
-            else {
-                $flags |= $STR_TO_UPDATE_FLAG{$method};
-            }
-        }
-        else {
+
+        if (! exists $STR_TO_UPDATE_FLAG{$method}) {
             ${$opts{-err}} = qq/"$method" is not a valid ARP update flag/;
             return;
         }
+
+        if ($negate) {
+            $flags &= ~ $STR_TO_UPDATE_FLAG{$method};
+            next;
+        }
+        $flags |= $STR_TO_UPDATE_FLAG{$method};
     }
     return $flags;
 }
 
-=item X<update_flags_to_str>B<update_flags_to_str> ( I<ARG> )
+=item B<update_flags_to_str> ( I<ARG> )
+X<update_flags_to_str>
 
 Translate the bits in I<ARG> to ARP update flag names and return a list of
 them.
@@ -181,7 +175,7 @@ them.
 =cut
 
 sub update_flags_to_str {
-    my $arg = shift;
+    my ($arg) = @_;
     my @list;
     
     if ($arg == ARP_UPDATE_NONE) {
