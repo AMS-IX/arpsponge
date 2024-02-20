6d3fec5d87ccebe0a25ae06d021ae279b3b930ba
Steven Bakker <steven.bakker@ams-ix.net>
2024-02-20 07:42:31 +0100

    Add asctl argument type for arp_update_flags.

---
+++ lib/M6/ArpSponge/Asctl/Arg_Arp_Update_Flags.pm
@@ -0,0 +1,52 @@
+package M6::ArpSponge::Asctl::Arg_Arp_Update_Flags;
+
+use Moo;
+
+extends 'Term::CLI::Argument';
+
+use Term::CLI::Util qw( find_text_matches );
+use M6::ArpSponge::Const qw( parse_update_flags );
+
+my @Flags = sort keys %M6::ArpSponge::Const::STR_TO_UPDATE_FLAG;
+
+use namespace::clean;
+
+around complete => sub {
+    my ($orig, $self, $text, $state) = @_;
+
+    my ($head, $partial) = $text =~ m{^(.*,)?(.*)};
+    $head //= '';
+
+    #if (!length $text) {
+    #   return ($self->$orig($text, $state), @Flags);
+    #}
+    return map { $head.$_ } find_text_matches( $partial, \@Flags );
+};
+
+sub translate {
+    my ($self, $text, $state) = @_;
+
+    #::DEBUG "translate: ", join(" ", map { "<$_>" } @_), "\n";
+
+    my $err;
+    my $flags = parse_update_flags(
+        $text, -err => \$err );
+
+    if (!defined $flags) {
+        return $self->set_error($err);
+    }
+
+    return $flags;
+}
+
+sub validate {
+    my ($self, $text, $state) = @_;
+
+    #::DEBUG "validate: ", join(" ", map { "<$_>" } @_), "\n";
+
+    my $flags = $self->translate($text, $state);
+    return if !defined $flags;
+    return $text;
+}
+
+1;
