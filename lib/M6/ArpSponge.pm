22f3b4dce1ca90059af55a75ecb53270c557f1cc
Steven Bakker <steven.bakker@ams-ix.net>
2024-04-21 08:54:47 -0400

    Add M6::ArpSponge to hold the package version.

---
+++ lib/M6/ArpSponge.pm
@@ -0,0 +1,76 @@
+#===============================================================================
+#
+#       Module:  M6::ArpSponge
+#
+#  Description:  See POD at end.
+#
+#       Author:  Steven Bakker (SB), <Steven.Bakker@ams-ix.net>
+#      Created:  2024-04-21
+#
+#   Copyright (c) 2024 AMS-IX B.V.; All rights reserved.
+#
+#   This module is free software; you can redistribute it and/or modify
+#   it under the same terms as Perl itself. See "perldoc perlartistic."
+#
+#   This software is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+#===============================================================================
+
+package M6::ArpSponge;
+
+use 5.014;
+use warnings;
+
+our $VERSION = '3.024006';
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+M6::ArpSponge - the AMS-IX ARP sponge
+
+=head1 SYNOPSIS
+
+ use M6::ArpSponge;
+
+ say $M6::ArpSponge::VERSION;
+
+=head1 DESCRIPTION
+
+This module is only used as a placeholder for the package version.
+
+The real magic happens in other modules in the C<M6::ArpSponge>
+name space, L<arpsponge>(8), and L<asctl>(8).
+
+=head1 VARIABLES
+
+=over
+
+=item I<$M6::ArpSponge::VERSION>
+
+The package's version.
+
+=head1 SEE ALSO
+
+L<arpsponge>(8), L<asctl>(8).
+
+=head1 AUTHOR
+
+Steven Bakker E<lt>Steven.Bakker@ams-ix.netE<gt>, AMS-IX B.V.; 2024.
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (c) 2024 AMS-IX B.V.; All rights reserved.
+
+This module is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself. See "perldoc perlartistic."
+
+This software is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
