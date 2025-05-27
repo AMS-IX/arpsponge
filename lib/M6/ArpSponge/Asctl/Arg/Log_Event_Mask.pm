eb0e06837bd03bfd676b1d27d41e12e740a49008
Steven Bakker <steven.bakker@ams-ix.net>
2025-05-27 17:15:45 +0200

    Add set log_mask

---
+++ lib/M6/ArpSponge/Asctl/Arg/Log_Event_Mask.pm
@@ -0,0 +1,49 @@
+package M6::ArpSponge::Asctl::Arg::Log_Event_Mask;
+
+use Moo;
+
+extends 'Term::CLI::Argument';
+
+use Term::CLI::Util qw( find_text_matches );
+use M6::ArpSponge::Event qw( event_names parse_event_mask );
+
+my @Events = event_names();
+
+use namespace::clean;
+
+around complete => sub {
+    my ($orig, $self, $text, $state) = @_;
+
+    my ($head, $partial) = $text =~ m{^(.*,)?(.*)};
+    $head //= '';
+
+    return map { $head.$_ } find_text_matches( $partial, \@Events );
+};
+
+sub translate {
+    my ($self, $text, $state) = @_;
+
+    #::DEBUG "translate: ", join(" ", map { "<$_>" } @_), "\n";
+
+    my $err;
+    my $mask = parse_event_mask(
+        $text, -err => \$err );
+
+    if (!defined $mask) {
+        return $self->set_error($err);
+    }
+
+    return $mask;
+}
+
+sub validate {
+    my ($self, $text, $state) = @_;
+
+    #::DEBUG "validate: ", join(" ", map { "<$_>" } @_), "\n";
+
+    my $mask = $self->translate($text, $state);
+    return if !defined $mask;
+    return $text;
+}
+
+1;
